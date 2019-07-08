6e0f98c818b66665d4c2454fdb0a44e97872ea0c
Frank van Woerden <frank@insyde.nl>
2019-07-08 10:16:51 +0200

    Project start

---
+++ Tutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tutorial
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int totalGuesses = 0;
+            const int gamesToPlay = 1;
+            GuessingGame guessingGame = new GuessingGame();
+
+            for (int i = 0; i < gamesToPlay; i++)
+            {
+                totalGuesses += guessingGame.Play(1, 200);
+            }
+
+            float averageGuesses = (float)totalGuesses / gamesToPlay;
+            Console.WriteLine($"the total number of guesses is {totalGuesses}");
+            Console.WriteLine($"average guesses per game is {averageGuesses}");
+        }
+    }
+}
